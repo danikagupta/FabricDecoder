e73b3bb66f7b6f4ae1380e4120862fe7fea25b3b
Danika Gupta <danika@gprof.com>
2024-02-11 17:40:10 -0800

    Initial Commit

---
+++ FabricDecoder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FabricDecoder
+//
+//  Created by Amit Gupta on 2/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FabricDecoder/FabricDecoderApp.swift
@@ -0,0 +1,17 @@
+//
+//  FabricDecoderApp.swift
+//  FabricDecoder
+//
+//  Created by Amit Gupta on 2/11/24.
+//
+
+import SwiftUI
+
+@main
+struct FabricDecoderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
